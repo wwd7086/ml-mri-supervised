8be1524bec7fbc4e199c15d061b2bf42d3c6cbb3
johnny <johnnywang@gmail.com>
2015-11-28 12:54:12 -0500

    The latest changes and values that got us to 64.8% accuracy.

--- mynormalize.m
+++ mynormalize.m
@@ -1,5 +1,12 @@
 function out = mynormalize(in)
 
+% h = fspecial('gaussian', [5 1], 0.5);
+% in = imfilter(in, h);
+
+% kernel = ones(3,3,3);
+% kernel = kernel / numel(kernel);
+% in = imfilter(in, kernel);
+
 out = bsxfun(@minus, in, mean(in,1));
 interval = max(out) - min(out);
 out = bsxfun(@rdivide, out, interval);
